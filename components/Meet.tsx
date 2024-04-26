1a34db7380fbc5f187ccbe5b0c4aba64da824d76
LimWeiJen <105616805+LimWeiJen@users.noreply.github.com>
2024-04-26 08:16:37 +0800

    fix: fix react hook conditional issues

--- components/Meet.tsx
+++ components/Meet.tsx
@@ -2,37 +2,42 @@
 
 import { tokenForUser } from '@/lib/actions/stream.actions'
 import { useUser } from '@clerk/nextjs'
-import { StreamVideoClient, StreamVideo, StreamCall, StreamTheme, PaginatedGridLayout, CallControls, User, StreamVideoProvider, SpeakerLayout, LivestreamLayout, CallStats, CallPreview, CallParticipantsList, CallStatsButton, CallState } from '@stream-io/video-react-sdk'
+import { StreamVideoClient, StreamVideo, StreamCall, StreamTheme, PaginatedGridLayout, CallControls, User, StreamVideoProvider, SpeakerLayout, LivestreamLayout, CallStats, CallPreview, CallParticipantsList, CallStatsButton, CallState, Call } from '@stream-io/video-react-sdk'
 import React, { useEffect, useState } from 'react'
 import Controls from './MeetControls'
 import MeetLayout from './MeetLayout'
 
 const Meet = ({ id }: { id: any }) => {
   const { isLoaded, isSignedIn, user } = useUser()
-
-  if (!isLoaded || !isSignedIn) return null;
-
-  const [client] = useState<StreamVideoClient>(() => {
-    const streamUser: User = {
-      id: user.id,
-      name: user.fullName!,
-      image: user.imageUrl
-    };
-    return new StreamVideoClient({
-      apiKey: process.env.NEXT_PUBLIC_STREAM_API_KEY!,
-      user: streamUser,
-      tokenProvider: tokenForUser,
-      options: { logLevel: "warn" }
-    })
-  })
-
-  const [call] = useState(() => client.call('default', id))
+  const [call, setcall] = useState<Call>();
+  const [client, setclient] = useState<StreamVideoClient>()
 
   useEffect(() => {
-    call.join({ create: true }).catch(err => {
-      console.error("Failed to join call", err)
-    })
-  }, [call]);
+
+    if (isLoaded && isSignedIn) {
+      let newClient = new StreamVideoClient({
+        apiKey: process.env.NEXT_PUBLIC_STREAM_API_KEY!,
+        user: {
+          id: user.id,
+          name: user.fullName!,
+          image: user.imageUrl
+        },
+        tokenProvider: tokenForUser,
+        options: { logLevel: "warn" }
+      })
+
+      let newCall = newClient!.call('default', id)
+
+      newCall!.join({ create: true }).catch(err => {
+        console.error("Failed to join call", err)
+      })
+
+      setcall(newCall)
+      setclient(newClient)
+    }
+  }, [isLoaded, isSignedIn, user])
+
+  if (!client || !call) return null
 
   return (
     <StreamVideo client={client}>
